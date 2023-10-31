ec6961584ee131f3ed9509e9ccbf2bbb113bb177
BDunnCode <BDunnCode@github.com>
2023-10-31 17:39:41 -0500

    layout game loop logic. begin adding bird.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+import { updateBird } from "./bird.js"
+
 document.addEventListener("keypress", handleStart, { once: true })
 const title = document.querySelector("[data-title]")
 
+let lastTime
 function updateLoop(time) {
-    console.log(time)
+    if (lastTime == null ) {
+        lastTime = time
+        window.requestAnimationFrame(updateLoop)
+        return
+    }
+    const delta = time - lastTime
+    updateBird(delta)
+    lastTime = time
+    window.requestAnimationFrame(updateLoop)
 }
 
 function handleStart() {
