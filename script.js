b740a93e5372e010775e56179d90ab77fde90ff2
BDunnCode <BDunnCode@github.com>
2023-11-01 00:37:47 -0500

    Nearing Completion. Bug with Pipes.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 import { updateBird, setupBird, getBirdRect } from './bird.js'
+import { updatePipes, setupPipes } from './pipe.js'
+
 
 document.addEventListener("keypress", handleStart, { once: true })
 const title = document.querySelector("[data-title]")
@@ -6,13 +8,14 @@ const subtitle = document.querySelector("[data-subtitle]")
 
 let lastTime
 function updateLoop(time) {
-    if (lastTime == null ) {
+    if (lastTime == null) {
         lastTime = time
         window.requestAnimationFrame(updateLoop)
         return
     }
     const delta = time - lastTime
     updateBird(delta)
+    updatePipes(delta)
     if (checkLose()) return handleLose()
     lastTime = time
     window.requestAnimationFrame(updateLoop)
@@ -27,6 +30,7 @@ function checkLose() {
 function handleStart() {
     title.classList.add("hide")
     setupBird()
+    setupPipes()
     lastTime = null
     window.requestAnimationFrame(updateLoop)
 }
