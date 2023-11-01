beca491f27cee94d79488ee0d395cc2734c61c0e
BDunnCode <BDunnCode@github.com>
2023-10-31 23:34:41 -0500

    various advancements

--- bird.js
+++ bird.js
@@ -1,7 +1,27 @@
 const birdElem = document.querySelector('[data-bird]')
+const BIRD_SPEED = 0.5;
+const JUMP_DURATION = 125
+let timeSinceLastJump = Number.POSITIVE_INFINITY
+
+
+export function setupBird() {
+    setTop(window.innerHeight / 2)
+    document.removeEventListener('keydown', handleJump)
+    document.addEventListener('keydown', handleJump)
+}
 
 export function updateBird(delta) {
-    console.log(getTop())
+    if (timeSinceLastJump < JUMP_DURATION) {
+    setTop(getTop() - BIRD_SPEED * delta)
+    } else {
+    setTop(getTop() + BIRD_SPEED * delta)
+    }
+
+    timeSinceLastJump += delta
+}
+
+export function getBirdRect() {
+    return birdElem.getBoundingClientRect()
 }
 
 function setTop(top) {
@@ -9,5 +29,11 @@ function setTop(top) {
 }
 
 function getTop() {
-    return getComputedStyle(birdElem).getPropertyValue("--bird-top")
+    return parseFloat(getComputedStyle(birdElem).getPropertyValue("--bird-top"))
+}
+
+function handleJump(e) {
+    if(e.code !== "Space") return
+
+    timeSinceLastJump = 0
 }
---
+++ pipe.js
@@ -0,0 +1,7 @@
+function createPipe() {
+    const pipeElem = document.createElement("div")
+}
+
+function createPipeSegment(position) {
+    
+}
